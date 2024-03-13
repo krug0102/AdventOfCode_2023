83f2e2914e95bb4037f2f13f785f797af38c93ce
Zeke Krug <krug0102@morris.umn.edu>
2024-03-13 14:39:36 -0500

    Finish Day 2: Cube Conundrum
    added the test game record and created a variable to hold the
    line returned by 'reader.nextline()'.

--- src/Day_2_Cube_Conundrum.java
+++ src/Day_2_Cube_Conundrum.java
@@ -3,56 +3,78 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 public class Day_2_Cube_Conundrum {
-    Pattern p = Pattern.compile("Game (\\d+): (\\d+) ([a-zA-Z]+)");
 
+    // TODO: Found solution to separate counts and colors into arrays here:
+    // https://stackoverflow.com/questions/6020384/create-array-of-regex-matches/46859130#46859130
+
+    public static void main(String[] args) {
+        prompt();
+    }
+
+    public static void prompt() {
+        System.out.println("Please provide the game records.");
+        Scanner s = new Scanner(System.in);
+        String fileName = s.nextLine();
+        System.out.println("The sum of the calibration values is: " + sumOfIDs(fileName));
+    }
 
     public static int sumOfIDs(String input) {
         int sum = 0;
-        int games = 0;
+        ArrayList<Game> games = new ArrayList<>();
         try {
             File inputFile = new File(input);
             Scanner reader = new Scanner(inputFile);
             while (reader.hasNextLine()) {
-                char[] line = reader.nextLine().toCharArray();
-                games++;
+                String line = reader.nextLine();
+                String[] numbers = Pattern.compile("(\\d+)")
+                        .matcher(line)
+                        .results()
+                        .map(MatchResult::group)
+                        .toArray(String[]::new);
+
+                String[] colors = Pattern.compile("([a-zA-Z]+)")
+                        .matcher(line)
+                        .results()
+                        .map(MatchResult::group)
+                        .toArray(String[]::new);
+
+                Game g = new Game(Integer.parseInt(numbers[0]));
+
+                for (int i = 1; i < numbers.length; i++) {
+                    if (colors[i].equals("red")) {
+                        g.redCount = g.redCount + Integer.parseInt(numbers[i]);
+                    }
+                    if (colors[i].equals("green")) {
+                        g.greenCount = g.greenCount + Integer.parseInt(numbers[i]);
+                    }
+                    if (colors[i].equals("blue")) {
+                        g.blueCount = g.blueCount + Integer.parseInt(numbers[i]);
+                    }
+                }
+
+                g.isPossible();
+
+                games.add(g);
 
             }
         } catch(FileNotFoundException e) {
             System.out.println(e);
         }
 
+        for (int i = 0; i < games.size(); i++) {
+            if (games.get(i).possible) {
+                sum = sum + games.get(i).id;
+            }
+        }
+
         return sum;
     }
 
-    public static void findPairs(char[] game) {
-        //ArrayList<ColorCountPair> pairs = new ArrayList<>();
-        Game g = new Game();
-        for (int i = 0; i < game.length; i++) {
-            if (Character.isDigit(game[i])) {
-                //pairs.add(new ColorCountPair(Character.getNumericValue(game[i]), game[i+2]));
-
-            }
-        }
-    }
-
-    public static void countColorPairs(ArrayList<ColorCountPair> colorCountPairs) {
-        for (int i = 0; i < colorCountPairs.size(); i++) {
-            // iterate over the color count pairs and create a new Game object which holds all of the
-        }
-    }
 
 
-    public static class ColorCountPair {
-        int count;  // The number of cubes pulled
-        Character color;  // an r, g, or b depending on the color of the cube
-
-        public ColorCountPair(int count, Character color) {
-            this.count = count;
-            this.color = color;
-        }
-    }
 
     public static class Game {
         int id;
@@ -62,12 +84,22 @@ public class Day_2_Cube_Conundrum {
 
         public Game() {}
 
+        public Game(int id) {
+            this.id = id;
+        }
+
         public Game(int id, int reds, int greens, int blues) {
             this.id = id;
             this.redCount = reds;
             this.greenCount = greens;
             this.blueCount = blues;
             this.possible = false;
+        }
+
+        public void isPossible() {
+            if (this.redCount <= 12 && this.greenCount <= 13 && this.blueCount <= 14) {
+                this.possible = true;
+            }
         }
     }
 }
