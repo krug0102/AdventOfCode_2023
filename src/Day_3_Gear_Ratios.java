12f9af4bc89eae74dc656cee749414bcbdbc6130
Zeke Krug <krug0102@morris.umn.edu>
2024-03-14 07:29:26 -0500

    Begin Day 3: Gear Ratios Problem

---
+++ src/Day_3_Gear_Ratios.java
@@ -0,0 +1,28 @@
+public class Day_3_Gear_Ratios {
+
+    // Can turn it into a 2-D char[][]
+    // look for symbols and check if any numbers are adjacent
+    // if a number is found, check to left and right of number
+
+
+
+    public static class PartNumber {
+        int number;
+
+        // x and y coordinate in 2-D array
+        int x;
+        int y;
+
+        boolean isPart;
+
+
+        public PartNumber(int n) {
+            this.number = n;
+        }
+
+        public static void isAdjacent() {
+            // given a number, looks at the adjacent (surrounding 9 spaces) and
+            // sees if there is a symbol
+        }
+    }
+}
