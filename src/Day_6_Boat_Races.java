31035156ec5fe5f21685cd72580432e57d5133b4
Zeke Krug <krug0102@morris.umn.edu>
2024-03-23 02:34:16 -0500

    Finish Day 6
    Finished Part 1 and Part 2 of Day 6: Wait For It
    Skipped Day 5 because it requires some extra thinking about the data
    structures I'm going to use

---
+++ src/Day_6_Boat_Races.java
@@ -0,0 +1,119 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class Day_6_Boat_Races {
+
+
+    public static void main(String[] args) {
+       prompt();
+    }
+
+    public static void prompt() {
+        System.out.println("Please provide race document.");
+        Scanner s = new Scanner(System.in);
+
+        String fileName = s.nextLine();
+        System.out.println("The product of the number of ways to beat the records is: "+ recordsProduct(fileName));
+
+        System.out.println("There are " + recordsProduct2(fileName) + " number of ways to beat the one, long race.");
+    }
+
+
+    public static int recordsProduct(String fileName) {
+        int product = 1;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        try {
+            File RaceDocument = new File(fileName);
+            Scanner reader = new Scanner(RaceDocument);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                int[] x = Pattern.compile("(\\d+)")
+                        .matcher(line)
+                        .results()
+                        .map(MatchResult::group)
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+                for (int i = 0; i < x.length; i++) {
+                    numbers.add(x[i]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        for (int i = 0; i < numbers.size()/2; i++) {
+            product = product * recordsBeat(numbers.get(i), numbers.get(i+(numbers.size()/2)));
+        }
+
+        return product;
+    }
+
+    public static int recordsBeat(int time, int record) {
+        int numberOfRecordsBeat = 0;
+        for (int i = 0; i < time; i++) {
+            int travelTime = time - i;
+            int distance = i * travelTime;
+
+            if (distance > record) {
+                numberOfRecordsBeat++;
+            }
+        }
+
+        return numberOfRecordsBeat;
+    }
+
+    public static long recordsProduct2(String fileName) {
+        long product = 1;
+        ArrayList<String> numbers = new ArrayList<>();
+
+        try {
+            File RaceDocument = new File(fileName);
+            Scanner reader = new Scanner(RaceDocument);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] x = Pattern.compile("(\\d+)")
+                        .matcher(line)
+                        .results()
+                        .map(MatchResult::group)
+                        .toArray(String[]::new);
+
+                Collections.addAll(numbers, x);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        StringBuilder time = new StringBuilder();
+        StringBuilder record = new StringBuilder();
+
+        for (int i = 0; i < numbers.size()/2; i++) {
+            time.append(numbers.get(i));
+            record.append(numbers.get(i+(numbers.size()/2)));
+        }
+
+       return recordsBeat2(Long.parseLong(String.valueOf(time)), Long.parseLong(String.valueOf(record)));
+    }
+
+    public static long recordsBeat2(long time, long record) {
+        long numberOfRecordsBeat = 0;
+        for (int i = 0; i < time; i++) {
+            long travelTime = time - i;
+            long distance = i * travelTime;
+
+            if (distance > record) {
+                numberOfRecordsBeat++;
+            }
+        }
+
+        return numberOfRecordsBeat;
+    }
+}
