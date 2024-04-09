f57c772d077520738fe42a2bd098ed35ea0ae91b
unknown <krug0102@morris.umn.edu>
2024-04-09 14:31:54 -0500

    Begin Day 5 Part 2
    Keep running out of memory on large input.  Need to think of new ways to
    use less memory

--- src/Day_5_Seeds.java
+++ src/Day_5_Seeds.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Array;
 import java.util.*;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -17,7 +18,9 @@ public class Day_5_Seeds {
         Scanner s = new Scanner(System.in);
         String fileName = s.nextLine();
 
-        System.out.println("The lowest location number that corresponds to an initial seed is " + part1(fileName));
+        System.out.println("Part 1: \nThe lowest location number that corresponds to an initial seed is " + part1(fileName));
+
+        System.out.println("Part 2: \nThe lowest location number that corresponds to an initial seed is " + part2(fileName));
     }
 
 
@@ -32,7 +35,6 @@ public class Day_5_Seeds {
 
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-
                 fileAsString.add(line);
             }
 
@@ -56,12 +58,11 @@ public class Day_5_Seeds {
                     .map(MatchResult::group)
                     .mapToLong(Long::parseLong)
                     .toArray();
-            System.out.println(Arrays.toString(arr));
             almanac.add(arr);
         }
 
         for (long[] map : almanac) {
-            destinationToSource(map, initialSeeds);
+            sourceToDestination(map, initialSeeds);
         }
 
         for (long location : initialSeeds) {
@@ -74,7 +75,137 @@ public class Day_5_Seeds {
     }
 
 
-    public static void destinationToSource(long[] map, long[] sourceMap) {
+    // TODO: Works on smaller data sets, like the example, but runs out of memory on larger sets, like the actual input
+    public static long part2(String fileName) {
+        long minLocation = Long.MAX_VALUE;
+        ArrayList<long[]> almanac = new ArrayList<>();
+        StringJoiner fileAsString = new StringJoiner(" ");
+
+        try {
+            File input = new File(fileName);
+            Scanner reader = new Scanner(input);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                fileAsString.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        String[] splitString = String.valueOf(fileAsString).split("([a-z]+-to-[a-z]+)? ?[a-z]+:");
+
+        long[] seedRanges = Pattern.compile("(\\d+)")
+                .matcher(splitString[1])
+                .results()
+                .map(MatchResult::group)
+                .mapToLong(Long::parseLong)
+                .toArray();
+
+        for (int i = 2; i < splitString.length; i++) {
+            long[] arr = Pattern.compile("(\\d+)")
+                    .matcher(splitString[i])
+                    .results()
+                    .map(MatchResult::group)
+                    .mapToLong(Long::parseLong)
+                    .toArray();
+            almanac.add(arr);
+        }
+
+        for (int i = 0; i < seedRanges.length; i+=2) {
+            long seedStart = seedRanges[i];
+            long range = seedRanges[i+1];
+            long[] initialSeeds = new long[(int)range];
+            for (int k = 0; k < range; k++) {
+                initialSeeds[k] = seedStart + k;
+            }
+            for (int j = 0; j < almanac.size(); j++) {
+                sourceToDestination(almanac.get(j), initialSeeds);
+            }
+            for (int l = 0; l < initialSeeds.length; l++) {
+                if (initialSeeds[l] < minLocation) {
+                    minLocation = initialSeeds[l];
+                }
+            }
+        }
+
+        return minLocation;
+    }
+
+
+    public static long part2v2(String fileName) {
+        long minLocation = Long.MAX_VALUE;
+        ArrayList<long[]> almanac = new ArrayList<>();
+        StringJoiner fileAsString = new StringJoiner(" ");
+
+        try {
+            File input = new File(fileName);
+            Scanner reader = new Scanner(input);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                fileAsString.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        String[] splitString = String.valueOf(fileAsString).split("([a-z]+-to-[a-z]+)? ?[a-z]+:");
+
+        long[] seedRanges = Pattern.compile("(\\d+)")
+                .matcher(splitString[1])
+                .results()
+                .map(MatchResult::group)
+                .mapToLong(Long::parseLong)
+                .toArray();
+
+        for (int i = 2; i < splitString.length; i++) {
+            long[] arr = Pattern.compile("(\\d+)")
+                    .matcher(splitString[i])
+                    .results()
+                    .map(MatchResult::group)
+                    .mapToLong(Long::parseLong)
+                    .toArray();
+            almanac.add(arr);
+        }
+
+        int size = 0;
+        for (int i = 0; i < seedRanges.length; i+=2) {
+            size = (int) (seedRanges[i+1] + size);
+        }
+
+        // size is 1,835,328,022 for Day 5 input.  Way too big
+        // Need to come up with a clever way to reduce the amount of ram being used
+        // Hashmap to store changes?
+
+        long[] initialSeeds = new long[size];
+        int index = 0;
+        for (int i = 0; i < seedRanges.length; i+=2) {
+            long start = seedRanges[i];
+            long range = seedRanges[i+1];
+            for (int j = 0; j < range; j++) {
+                initialSeeds[index] = start + j;
+                index++;
+            }
+        }
+
+
+        for (int i = 0; i < almanac.size(); i++) {
+            sourceToDestination(almanac.get(i), initialSeeds);
+        }
+
+        for (int i = 0; i < initialSeeds.length; i++) {
+            if (initialSeeds[i] < minLocation) {
+                minLocation = initialSeeds[i];
+            }
+        }
+
+        return minLocation;
+    }
+
+
+
+    public static void sourceToDestination(long[] map, long[] sourceMap) {
         for (int i = 0; i < sourceMap.length; i++) {
             long x = sourceMap[i];
             for (int j = 0; j < map.length; j+=3) {
@@ -90,5 +221,7 @@ public class Day_5_Seeds {
             }
         }
     }
+
+
 
 }
